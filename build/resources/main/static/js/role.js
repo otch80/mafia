09d8b9d48bb4037ce04aff7b54e302187492365d
dundole <dundole@naver.com>
2020-11-20 14:33:17 +0900

    새로운 소스코드입니다2

--- build/resources/main/static/js/role.js
+++ build/resources/main/static/js/role.js
@@ -9,15 +9,18 @@ var role = {
         group: 'citizen',
         ability: true,
         abilityFunc: function (server, chosenPlayer) {//서버로부터 선택할 플레이어정보 받아옴
-            if (/*지목한 플레이어가 죽었는가? 아니라면 실행*/) {
-                if (chosenPlayer == 'mafia') { //지목한 플레이어의 상태가 마피아라면
+            if (chosenPlayer.status == 1/* status가 1이면 생존 */) {
+                if (chosenPlayer.name == 'mafia') { //지목한 플레이어의 상태가 마피아라면
+                    showMafia(1);
                     //화면상에 마피아임을 알려주는 애니메이션 실행
                 }
                 else {
+                    showMafia(0);
                     //화면상에 마피아가 아님을 알려주는 애니메이션 실행
                 }
             }
             else {
+                alert("현재 생존해있지 않습니다.");
                 //현재 플레이어가 생존해있지 않음을 알려주는 애니메이션 실행
             }
         }
@@ -27,10 +30,11 @@ var role = {
         group: 'citizen',
         ability: true,
         abilityFunc: function (server, chosenPlayer) {//서버로 부터 플레이어정보 받아옴
-            if (/*지목한 플레이어가 죽었는가? 아니라면 실행*/) {
+            if (chosenPlayer.status == 1/*지목한 플레이어가 죽었는가? 아니라면 실행*/) {
                 chosenPlayer.imperishability = true; //플레이어의 상태를 무적으로 바꾸어 마피아로 부터 보호
             }
             else {
+                alert("현재 생존해있지 않습니다.");
                 //현재 플레이어가 생존해있지 않음을 알려주는 애니메이션 실행
             }
         }
@@ -39,7 +43,7 @@ var role = {
         name: 'priest',
         group: 'citizen',
         ability: false, // 성직자의 능력은 함수로 구현하지 않고 상태변화로 구현
-        status : 1
+        barrier : true
     },
     mafia: {
         name: 'mafia',
@@ -51,15 +55,26 @@ var role = {
         group: 'citizen', // spy의 성격상 마피아를 지목하지 않으면 시민팀이기 때문에 처음엔 citizen으로 배정함
         ability: true,
         abilityFunc: function (server, chosenPlayer) {
-            if (/*지목한 플레이어가 죽었는가? 아니라면 실행*/) {
+            if (chosenPlayer.status == 1/*지목한 플레이어가 죽었는가? 아니라면 실행*/) {
                 if (chosenPlayer.name == 'mafia') {//선택한 플레이어의 이름이 마피아가 맞으면 실행
                     this.group = 'mafia' // 현재플레이어는 마피아그룹이됨 이부분코드는 나중에 손봐줘야함
+                    alert("당신은 이제부터 마피아입니다.");
                 }
             }
             else {
+                alert("현재 생존해있지 않습니다.");
                 //현재 플레이어가 생존해있지 않음을 알려주는 애니메이션 실행
             }
         }
+    },
+    showMafia(num) {
+        if(num==1){
+            alert("마피아가 맞습니다.");
+        }
+        else{
+            alert("마피아가 아닙니다.");
+        }
+
     }
 }
 
